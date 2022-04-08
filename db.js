02fef63a834211b6d3ba5430a4648898f2f8d3ea
James Cinense <cinensejames@gmail.com>
2022-04-08 11:07:45 +0800

    model, app, db

---
+++ db.js
@@ -0,0 +1,24 @@
+const { Sequelize, DataTypes } = require('sequelize')
+const EmployeeModel = require('./models/employee.model')
+
+const sequelize = new Sequelize({
+    dialect: 'mysql',
+    database: 'hackathon',
+    username: 'root',
+    password: '',
+    host: 'localhost',
+    port: '3306'
+})
+
+const Employee = EmployeeModel(sequelize, DataTypes)
+
+if (false) {
+    sequelize.sync()
+        .then(() => {
+            console.log('Database sync completed.')
+        })
+}
+
+module.exports = {
+    Employee
+}
